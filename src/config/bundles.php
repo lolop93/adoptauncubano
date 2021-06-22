7bb5a65bc58fbbb0d08faaaac0401481b368def1
lolop93 <lolop_1993@hotmail.com>
2021-06-22 19:53:24 +0200

    Añadido easyadmin

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
