477e7d65281f5c5a35e5c1f81f3785f481aa76ff
lolop93 <lolop_1993@hotmail.com>
2021-06-24 01:13:38 +0200

    Añadido paquetes Debug (dev) y logger
    - symfony composer require debug --dev
    - symfony require logger
    
    Bundles actualizados

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
