8d6311799e14370fed57bf6c454c925994db4e44
lolop93 <lolop_1993@hotmail.com>
2021-06-28 13:42:31 +0200

    Añadido bootstrap y webpack encore con sass loader

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -12,4 +12,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
