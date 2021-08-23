b6fb2154d1158a89fa7cf787e584c9f568b7f657
lolop93 <lolop_1993@hotmail.com>
2021-08-23 11:03:41 +0200

    navegacion fixed

--- src/src/Security/AppAuthenticator.php
+++ src/src/Security/AppAuthenticator.php
@@ -95,7 +95,7 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('easyadmin'));
+        return new RedirectResponse($this->urlGenerator->generate('homepage'));
 
     }
 
