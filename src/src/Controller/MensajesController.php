7012a0beefc5c683e470e181188c2155f39dd73d
lolop93 <lolop_1993@hotmail.com>
2021-08-31 14:57:32 +0200

    ajax pa mensajes

--- src/src/Controller/MensajesController.php
+++ src/src/Controller/MensajesController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MensajesController extends AbstractController
 {
-    #[Route('/mensajes', name: 'mensajes')]
-    public function index(): Response
+    /**
+     * @Route("/mensajes", name="mensajes", options={"expose"=true})
+     */
+    public function index(Request $request): Response
     {
-        return $this->render('mensajes/index.html.twig', [
-            'controller_name' => 'MensajesController',
-        ]);
+        if($request->isXMLHttpRequest()){
+            $user = $this->getUser();
+            $id = $user->getId();
+            return new JsonResponse(['id'=>$id]);
+
+
+        }else{
+            return $this->render('mensajes/index.html.twig', [
+                'controller_name' => 'MensajesController',
+            ]);
+            //throw new Exception("Holi :)");
+        }
+
+
     }
 }
