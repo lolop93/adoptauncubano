2d53ed893eb63531383c91f92d22e510dd1a56d6
lolop93 <lolop_1993@hotmail.com>
2021-09-02 05:09:07 +0200

    busqueda 0.3

---
+++ src/src/Controller/BusquedaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BusquedaController extends AbstractController
+{
+    #[Route('/busqueda', name: 'busqueda', methods: ['GET'])]
+    public function index(Request $request): Response
+    {
+        $login = $this->get('security.token_storage')->getToken()->getUser();
+        $query = $request->query->get('busqueda');
+
+
+        return $this->render('busqueda/index.html.twig', [
+            'query' => $query,
+            'request'=> $request,
+            'login' => $login,
+        ]);
+    }
+
+    public function barraBusqueda(){
+
+        $form = $this->createFormBuilder(null)
+            ->setAction($this->generateUrl('busqueda'))
+            ->setMethod('GET')
+            ->add('query', TextType::class)
+            ->add('submit',SubmitType::class)
+            ->getForm();
+
+        /*$form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Los datos están en un array con los keys "name", "email", y "message"
+            $data = $form->getData();
+         }*/
+
+        return $this->render('busqueda/barraBusqueda.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+}
--- src/src/Controller/SecurityController.php
+++ src/src/Controller/SecurityController.php
@@ -34,8 +34,5 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
-    public function pruebas(): Response
-    {
-        return $this->render('security/pruebas.html.twig');
-    }
+
 }
