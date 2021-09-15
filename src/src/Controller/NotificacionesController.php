8879e2e219f7e3a9746c5b03112a2c09ee011003
jaime <james.defen95@gmail.com>
2021-09-15 13:45:18 +0200

    notificaciones

---
+++ src/src/Controller/NotificacionesController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ConversacionesRepository;
+use App\Repository\GaleriaRepository;
+use App\Repository\MensajesRepository;
+use App\Repository\UserAttributesRepository;
+use App\Repository\UserRepository;
+use SunCat\MobileDetectBundle\DeviceDetector\MobileDetector;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NotificacionesController extends AbstractController
+
+{
+    #[Route('/notificaciones', name: 'notificaciones')]
+    public function notificaciones(MobileDetector $pantalla, UserRepository $userRepository, GaleriaRepository $galeriaRepository, UserAttributesRepository $userAttributesRepository,ConversacionesRepository $conversacionesRepository,MensajesRepository $mensajesRepository): Response
+    {
+
+        $login = $this->get('security.token_storage')->getToken()->getUser();
+
+        $users = $userRepository->findAll();
+        $mensajes = $mensajesRepository->findAll();
+        $conversaciones = $conversacionesRepository->findAll();
+
+        $galerias = $galeriaRepository->findAll();
+        $atributos = $userAttributesRepository->findAll();
+
+        $mensajeslogin =  $login->getMensajes();
+
+        if($pantalla->isMobile() && !$pantalla->isTablet()){
+            return $this->render('notificaciones/index.mobile.html.twig', [
+                'users' => $users,
+                'galerias' => $galerias,
+                'atributos' => $atributos,
+                'login' => $login,
+            ]);
+        }else {
+            return $this->render('homepage/index.html.twig', [
+                'users' => $users,
+                'galerias' => $galerias,
+                'atributos' => $atributos,
+                'login' => $login,
+                'conversaciones' => $conversaciones,
+                'mensajes' => $mensajeslogin,
+                'todosmensajes' => $mensajes,
+            ]);
+        }
+
+    }
+
+
+}
