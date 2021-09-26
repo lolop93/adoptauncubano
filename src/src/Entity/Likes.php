4415f963ce46d55753c39b6952333f6adf50447b
lolop93 <lolop_1993@hotmail.com>
2021-09-27 01:26:09 +0200

    Revert "New changelist"

---
+++ src/src/Entity/Likes.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LikesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LikesRepository::class)
+ */
+class Likes
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="likesRecibidos")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $likesTo;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="likesDados")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $likesFrom;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $fecha;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLikesTo(): ?User
+    {
+        return $this->likesTo;
+    }
+
+    public function setLikesTo(?User $likesTo): self
+    {
+        $this->likesTo = $likesTo;
+
+        return $this;
+    }
+
+    public function getLikesFrom(): ?User
+    {
+        return $this->likesFrom;
+    }
+
+    public function setLikesFrom(?User $likesFrom): self
+    {
+        $this->likesFrom = $likesFrom;
+
+        return $this;
+    }
+
+    public function getFecha(): ?\DateTimeInterface
+    {
+        return $this->fecha;
+    }
+
+    public function setFecha(\DateTimeInterface $fecha): self
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+}
