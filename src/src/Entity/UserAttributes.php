eeedfea90de6a222e2288da333fa94fad9011446
lolop93 <lolop_1993@hotmail.com>
2021-10-21 20:14:35 +0200

    Atributos creados

--- src/src/Entity/UserAttributes.php
+++ src/src/Entity/UserAttributes.php
@@ -32,6 +32,51 @@ class UserAttributes
      */
     private $usuario;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nacionalidad;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $fecha_nac;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $ojos;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $profesion;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $ciudad;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $altura;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $peso;
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $gustos = [];
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $sexo;
+
 
     /**
      * @param mixed $id
@@ -84,6 +129,114 @@ class UserAttributes
         }
 
         $this->usuario = $usuario;
+
+        return $this;
+    }
+
+    public function getNacionalidad(): ?string
+    {
+        return $this->nacionalidad;
+    }
+
+    public function setNacionalidad(?string $nacionalidad): self
+    {
+        $this->nacionalidad = $nacionalidad;
+
+        return $this;
+    }
+
+    public function getFechaNac(): ?\DateTimeInterface
+    {
+        return $this->fecha_nac;
+    }
+
+    public function setFechaNac(?\DateTimeInterface $fecha_nac): self
+    {
+        $this->fecha_nac = $fecha_nac;
+
+        return $this;
+    }
+
+    public function getOjos(): ?string
+    {
+        return $this->ojos;
+    }
+
+    public function setOjos(?string $ojos): self
+    {
+        $this->ojos = $ojos;
+
+        return $this;
+    }
+
+    public function getProfesion(): ?string
+    {
+        return $this->profesion;
+    }
+
+    public function setProfesion(?string $profesion): self
+    {
+        $this->profesion = $profesion;
+
+        return $this;
+    }
+
+    public function getCiudad(): ?string
+    {
+        return $this->ciudad;
+    }
+
+    public function setCiudad(?string $ciudad): self
+    {
+        $this->ciudad = $ciudad;
+
+        return $this;
+    }
+
+    public function getAltura(): ?int
+    {
+        return $this->altura;
+    }
+
+    public function setAltura(?int $altura): self
+    {
+        $this->altura = $altura;
+
+        return $this;
+    }
+
+    public function getPeso(): ?int
+    {
+        return $this->peso;
+    }
+
+    public function setPeso(?int $peso): self
+    {
+        $this->peso = $peso;
+
+        return $this;
+    }
+
+    public function getGustos(): ?array
+    {
+        return $this->gustos;
+    }
+
+    public function setGustos(?array $gustos): self
+    {
+        $this->gustos = $gustos;
+
+        return $this;
+    }
+
+    public function getSexo(): ?string
+    {
+        return $this->sexo;
+    }
+
+    public function setSexo(?string $sexo): self
+    {
+        $this->sexo = $sexo;
 
         return $this;
     }
