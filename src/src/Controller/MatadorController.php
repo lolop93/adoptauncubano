0cd59d229ef43ba35055df69c692a613f3c3de04
jaimepdg <78267768+jaimepdg@users.noreply.github.com>
2022-01-27 18:31:00 +0100

    fix formulario editar ajustes y ajuste de MatadorController.php

--- src/src/Controller/MatadorController.php
+++ src/src/Controller/MatadorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use SunCat\MobileDetectBundle\DeviceDetector\MobileDetector;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class MatadorController extends AbstractController
 {
     #[Route('/matador', name: 'matador')]
-    public function index(): Response
+    public function index(MobileDetector $pantalla): Response
     {
-        return $this->render('matador/index.html.twig', [
-            'controller_name' => 'MatadorController',
-        ]);
+        $login = $this->get('security.token_storage')->getToken()->getUser();
+
+        if($pantalla->isMobile() && !$pantalla->isTablet()){
+            return $this->render('matador/matadorMobile.html.twig', [
+                'login' => $login,
+            ]);
+        }else {
+            return $this->render('matador/matador.html.twig', [
+                'login' => $login,
+            ]);
+        }
     }
 }
