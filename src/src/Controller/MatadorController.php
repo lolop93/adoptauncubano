98e57feaea64a7faff5086caad85c4b59761ecad
Manuel Prieto Macias <lolop_1993@hotmail.com>
2022-02-15 02:03:40 +0100

    matador 0.6

--- src/src/Controller/MatadorController.php
+++ src/src/Controller/MatadorController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use SunCat\MobileDetectBundle\DeviceDetector\MobileDetector;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,17 +12,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class MatadorController extends AbstractController
 {
     #[Route('/matador', name: 'matador')]
-    public function index(MobileDetector $pantalla): Response
+    public function index(MobileDetector $pantalla, UserRepository $userRepository): Response
     {
         $login = $this->get('security.token_storage')->getToken()->getUser();
+        $likesRecibidos = $login->getLikesRecibidos();
+        $users = $userRepository->findAll();
+        $contadorLikesMatador = 0;
+        $contadorLikesMatadora = 0;
+        $matadora = new User();
+        $matador = new User();
+
+        foreach ($users as $user){
+            if($user->getAtributos() && $user->getAtributos()->getSexo() ){
+                if($user->getAtributos()->getSexo()=="chico"){
+                    if($user->getLikesRecibidos()->count() > $contadorLikesMatador){
+                        $matador = $user;
+                    }
+                }else{
+                    if($user->getLikesRecibidos()->count() > $contadorLikesMatadora){
+                        $matadora = $user;
+                    }
+                }
+            }
+        }
 
         if($pantalla->isMobile() && !$pantalla->isTablet()){
             return $this->render('matador/matadorMobile.html.twig', [
                 'login' => $login,
+                'matador' => $matador,
+                'matadora' => $matadora,
             ]);
         }else {
             return $this->render('matador/matador.html.twig', [
                 'login' => $login,
+                'matador' => $matador,
+                'matadora' => $matadora,
             ]);
         }
     }
